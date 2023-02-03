2270f59fda2dc99b66fb2e00ab709c5d6e37a7cb
Lzdavic <lzdavicc@gmail.com>
2023-02-02 22:37:54 -0300

    Add and remove ingredients code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,43 @@
-const form = document.getElementById('#form');
-const ingredientList = document.getElementById
+const form = document.getElementById("form");
+const select = document.getElementById("ingredient-unity");
 
-if (form) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+// Update dropdown menu value when it's selection changes
+select.addEventListener("change", () => {
+    unity = select.value;
+});
 
-        const ingredientName = form.getElementById('#ingredient-name').value;
-        const ingredientQuantity = form.getElementById('#ingredient-quantity').value;
-        const ingredientUnity = form.getElementById('#quantity-type').value;
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        createIngredient(ingredientName, ingredientQuantity, ingredientUnity);
-    });
-}
+    const name = document.getElementById("ingredient-name").value;
+    const quantity = document.getElementById("ingredient-quantity").value;
 
-function createIngredient(name, quantity, unity) {
-    var ingredientItem = `<li class="ingredients__item">
-                        <button id="exclude" class="exclude-button">X</button>
-                        <p>${name} ${quantity}${unity}</p>
-                        </li>`
+    const newLi = document.createElement("li");
+    newLi.classList.add("ingredients__item");
+    newLi.innerHTML = `
+        <button id="exclude" class="exclude-button">X</button>
+        <p><span id=item-name>${name}</span> - <span id="item-quantity">${quantity}</span> <span id="item-unity">${unity}</span></p>`;
 
-    ingredientList.appendChild = ingredientItem
-}   
+    const list = document.getElementById("ingredients");
+    list.appendChild(newLi);
+
+    
+
+    // exclude each item
+    const excludeButtons = document.querySelectorAll(".exclude-button");
+    excludeButtons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            this.parentElement.remove();
+        })
+    })
+
+    // Loop over each input and clear its value
+    const inputs = form.querySelectorAll("input");
+    inputs.forEach((input) => {
+        input.value = "";
+    })
+
+    const add = document.getElementById("button-add");
+    add.value = "ADD"
+});
